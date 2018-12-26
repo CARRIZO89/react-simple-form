8a735178aad1921a77b34f3b9619c7e4eb7ddfd7
Miguel <Miguel Carrizo>
2018-12-26 17:31:28 -0300

    [apply-test]
      -Refactoried test to show no contact when the store is empty in src/__tests__/components/ContactList
      -Added test to show a contact in src/__tests__/components/ContactList

--- src/__tests__/components/ContactList.js
+++ src/__tests__/components/ContactList.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import { ContactList } from '../../components/ContactList';
-import { shallow } from 'enzyme';
+import ConnectedContactList from '../../components/ContactList';
+import { render } from 'enzyme';
+import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store'
 import enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 enzyme.configure({ adapter: new Adapter() });
 
+const mockStore = configureStore();
+
 it('renders no contacts when store is empty', () => {
-  const wrapper = shallow(<ContactList contactList={[]}/>);
+  const store = mockStore({ contactList: [] });
+
+  const wrapper = render(
+    <Provider store={store}>
+      <ConnectedContactList />
+    </Provider>);
   expect(wrapper.find("#contact").length).toBe(0);
 });
+
+it('render contact', () => {
+  const store = mockStore ({
+    contactList: [{
+      name: 'Miguel Antonio',
+      country: 'Argentina',
+      birthday: '02/05/1989'}]
+  });
+
+  const wrapper = render(
+    <Provider store={store}>
+      <ConnectedContactList/>
+    </Provider>);
+  expect(wrapper.find("#contact").length).toBe(1);
+});
+// Para la versión 6, en adelante, de "react-redux" se utiliza el concepto de "context"
+// es por eso q el "store" se lo pasa de esta manera al componente a testear:
+// <Provider store={store}>
+//   <ConnectedComponent/>
+// </Provider>
