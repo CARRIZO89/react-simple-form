69508729227e2dec47363608b46d96436033fa76
Miguel <Miguel Carrizo>
2019-01-10 17:40:11 -0300

    [combine-reducer-function]
      -Removed console.log in src/components/WelcomeMessage
      -Added reducer current_contact in src/store

--- src/store.js
+++ src/store.js
@@ -4,9 +4,8 @@ import thunk from 'redux-thunk';
 const contactList = (state = [], action) => {
   if (action.type === "ADD_TO_LIST") {
     return state.concat(action.contact)
-  } else {
-    return action.current_contact
   }
+
   return state
 };
 
@@ -14,9 +13,18 @@ const countries = (state = [], action) => {
   if (action.type === "LOAD_COUNTRIES") {
     return action.countries
   }
+
+  return state
+};
+
+const current_contact = (state = {}, action) => {
+  if (action.type === "SET_CURRENT_CONTACT") {
+    return action.contact
+  }
+
   return state
 };
 
 export default createStore(
-  combineReducers({ contactList, countries }),applyMiddleware(thunk)
+  combineReducers({ contactList, countries, current_contact }),applyMiddleware(thunk)
 );
